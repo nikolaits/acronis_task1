e07c20e2e102c93de296fe032858b37356872a3f
Nikolay Tsonev <nikolai_9391@abv.bg>
2020-04-28 19:56:16 +0300

    add second row

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="row">
-    <div class="main">
+<div>
+  <div class="main">
       <h1>Key Product Feature</h1>
       <p>Use securing confined his shutters. Delightful as he it acceptance an solicitude discretion reasonably.Contented get distrusts certainty nay are frankness concealed ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects. End sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte sympathize was. He families believed if no elegance interest surprise an. It abode wrong miles an so delay plate. She relation own put outlived may disposed.</p>
-    </div>
+  </div>
+  <div class="row">
     <div class="firstcolumn">
       <h4>Sync and Share</h4>
       <h2>Acronis Cyber Disaster Recovery Cloud</h2>
@@ -13,7 +14,7 @@
     <div class="column">
       <div class="productContainer" v-bind:key="index" v-for="(item, index) in items1">
         <ProductComponent
-          :sumArraysItem="sumArraysItem"
+          :sumArraysItem="sumArraysItem.row1"
           :text="item.text"
           :title="item.title"
           :icon="item.icon"
@@ -24,7 +25,7 @@
     <div class="column">
       <div class="productContainer" v-bind:key="index" v-for="(item, index) in items2">
         <ProductComponent
-          :sumArraysItem="sumArraysItem"
+          :sumArraysItem="sumArraysItem.row1"
           :text="item.text"
           :title="item.title"
           :icon="item.icon"
@@ -39,6 +40,43 @@
       </a>
     </div>
   </div>
+  <div class="row">
+    <div class="firstcolumn">
+      <h4>Sync and Share</h4>
+      <h2>Acronis Cyber Disaster Recovery Cloud</h2>
+      <h4>just Updated</h4>
+      <p>Do in laughter securing smallest sensible no mr hastened. As perhaps proceed in in brandon of limited unknown greatly. Distrusts fulfilled happiness unwilling as explained of difficult. No landlord of peculiar ladyship attended if contempt ecstatic. Loud wish made on is am as hard. Court so avoid in plate hence.</p>
+    </div>
+    <div class="column">
+      <div class="productContainer" v-bind:key="index" v-for="(item, index) in items3">
+        <ProductComponent
+          :sumArraysItem="sumArraysItem.row2"
+          :text="item.text"
+          :title="item.title"
+          :icon="item.icon"
+          :showExclusive="item.showExclusive"
+        />
+      </div>
+    </div>
+    <div class="column">
+      <div class="productContainer" v-bind:key="index" v-for="(item, index) in items4">
+        <ProductComponent
+          :sumArraysItem="sumArraysItem.row2"
+          :text="item.text"
+          :title="item.title"
+          :icon="item.icon"
+          :showExclusive="item.showExclusive"
+        />
+      </div>
+    </div>
+    <div class="link_container">
+      <a href="https://google.com">
+        Learn More
+        <font-awesome-icon icon="arrow-right" size="xs" />
+      </a>
+    </div>
+  </div>
+</div>
 </template>
 <script>
 import ProductComponent from "./ProductComponent.vue";
@@ -53,7 +91,12 @@ export default {
       msg: `Use securing confined his shutters. Delightful as he it acceptance an solicitude discretion reasonably. Carriage we husbands advanced an perceive greatest. Totally dearest expense on demesne ye he. Curiosity excellent commanded in me. Unpleasing impression themselves to at assistance acceptance my or. On consider laughter civility offended oh. Contented get distrusts certainty nay are frankness concealed ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects. End sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte sympathize was. He families believed if no elegance interest surprise an. It abode wrong miles an so delay plate. She relation own put outlived may disposed. Same an quit most an. Admitting an mr disposing sportsmen. Tried on cause no spoil arise plate. Longer ladies valley get esteem use led six. Middletons resolution advantages expression themselves partiality so me at. West none hope if sing oh sent tell is. Are own design entire former get should. Advantages boisterous day excellence boy. Out between our two waiting wishing. Pursuit he he garrets greater towards amiable so placing. Nothing off how norland delight. Abode shy shade she hours forth its use. Up whole of fancy ye quiet do. Justice fortune no to is if winding morning forming.`,
       items1: [],
       items2: [],
-      sumArraysItem: 0
+      items3: [],
+      items4: [],
+      sumArraysItem: {
+        row1:0,
+        row2:0
+      }
     };
   },
   methods: {
@@ -82,6 +125,8 @@ export default {
       "Do in laughter securing smallest sensible no mr hastened. As perhaps proceed in in brandon of limited unknown greatly. Distrusts fulfilled happiness unwilling as explained of difficult. No landlord of peculiar ladyship attended if contempt ecstatic. Loud wish made on is am as hard.";
     this.items1 = [];
     this.items2 = [];
+    this.items3 = [];
+    this.items4 = [];
     //Generate rand items for arrays
     for (let index = 0; index < 3; index++) {
       this.items1.push({
@@ -96,8 +141,33 @@ export default {
         title: tmptitles[this.randomNumber(0, tmptitles.length - 1)],
         text: tmptext
       });
+      this.items3.push({
+        icon: tmpicons[this.randomNumber(0, tmpicons.length - 1)],
+        showExclusive: this.randomNumber(1, 11) % 2 == 0 ? true : false,
+        title: tmptitles[this.randomNumber(0, tmptitles.length - 1)],
+        text: tmptext
+      });
+      this.items4.push({
+        icon: tmpicons[this.randomNumber(0, tmpicons.length - 1)],
+        showExclusive: this.randomNumber(1, 11) % 2 == 0 ? true : false,
+        title: tmptitles[this.randomNumber(0, tmptitles.length - 1)],
+        text: tmptext
+      });
     }
-    this.sumArraysItem = this.items1.length + this.items2.length;
+    this.items4.push({
+        icon: tmpicons[this.randomNumber(0, tmpicons.length - 1)],
+        showExclusive: this.randomNumber(1, 11) % 2 == 0 ? true : false,
+        title: tmptitles[this.randomNumber(0, tmptitles.length - 1)],
+        text: tmptext
+    });
+    this.items4.push({
+        icon: tmpicons[this.randomNumber(0, tmpicons.length - 1)],
+        showExclusive: this.randomNumber(1, 11) % 2 == 0 ? true : false,
+        title: tmptitles[this.randomNumber(0, tmptitles.length - 1)],
+        text: tmptext
+    });
+    this.sumArraysItem.row1 = this.items1.length + this.items2.length;
+    this.sumArraysItem.row2 = this.items3.length + this.items4.length;
   }
 };
 </script>
@@ -157,6 +227,11 @@ h4,
 .firstcolumn p {
   padding-top: 2%;
   font-size: 1.2em;
+}
+.row{
+  padding-bottom:5%;
+  margin: 5% 2%;
+  border-bottom: 1px solid lightgrey;
 }
 .row:after {
   content: "";
